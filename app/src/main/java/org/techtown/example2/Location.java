b53e624db42f87b6ee88646e99173913ec6930ca
gks54 <gks54410@gmail.com>
2021-02-25 19:53:14 +0900

    change text to hint 위도 경도 주소로 변경

--- app/src/main/java/org/techtown/example2/Location.java
+++ app/src/main/java/org/techtown/example2/Location.java
@@ -4,9 +4,12 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,23 +17,34 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class Location extends AppCompatActivity {
 
     private int MY_PERMISSIONS_REQUEST_LOCATION = 10;
-    TextView status;
+    TextView status, status2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_location);
         status = findViewById(R.id.status);
+        status2 = findViewById(R.id.status2);
+
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(android.location.Location location) {
-                status.setText("위도; " + location.getLatitude() + "\n경도:"
+                status.setText("위도: " + location.getLatitude() + "\n경도:"
                         + location.getLongitude() + "\n고도:"
                         + location.getAltitude());
+                getLocation(location.getLatitude(), location.getLongitude());
             }
 
             public void onStatusChanged(String provider, int status,
@@ -47,9 +61,31 @@ public class Location extends AppCompatActivity {
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, locationListener);
     }
-    public void cilckout(View view){
+
+    public void cilckout(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
+    public void getLocation(double lat, double lng){
+        String str = null;
+        Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+
+        List<Address> address;
+        try {
+            if (geocoder != null) {
+                address = geocoder.getFromLocation(lat, lng, 1);
+                if (address != null && address.size() > 0) {
+                    str = address.get(0).getAddressLine(0).toString();
+                }
+            }
+        } catch (IOException e) {
+            Log.e("MainActivity", "주소를 찾지 못하였습니다.");
+            e.printStackTrace();
+        }
+        status2.setText("주소 : " + str);
+    }
+
 }
+
+
